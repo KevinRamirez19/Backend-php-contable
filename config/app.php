cc66bbb7523642963a37a0567ea0855632466220
Kevin200519 <Ksramirez@ucundinamarca.edu.co>
2025-11-12 16:49:08 -0500

    Actualizacion

--- config/app.php
+++ config/app.php
@@ -35,7 +35,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
